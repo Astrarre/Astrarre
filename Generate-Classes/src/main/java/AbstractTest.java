7012fc9b6949af855e2597469007305c8d9c22a6
Devan-Kerman <dev.sel20@gmail.com>
2020-12-04 22:48:18 -0600

    unholy gradle script
    Signed-off-by: Devan-Kerman <dev.sel20@gmail.com>

--- Generate-Classes/src/main/java/AbstractTest.java
+++ Generate-Classes/src/main/java/AbstractTest.java
@@ -10,6 +10,7 @@ import io.github.astrarre.abstracter.AbstracterUtil;
 import io.github.astrarre.abstracter.abs.ConstantsAbstracter;
 import io.github.astrarre.abstracter.abs.InterfaceAbstracter;
 import io.github.astrarre.abstracter.util.AbstracterLoader;
+import org.reflections.Reflections;
 
 import net.minecraft.Bootstrap;
 import net.minecraft.block.AbstractBlock;
@@ -18,16 +19,20 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.block.MaterialColor;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemSteerable;
+import net.minecraft.entity.SaddledComponent;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.Tag;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
@@ -40,12 +45,13 @@ import net.minecraft.world.WorldAccess;
 		"ResultOfMethodCallIgnored"
 })
 public class AbstractTest {
+	private static final Reflections REFLECTIONS = new Reflections();
 	private static final Properties PROPERTIES = new Properties();
 
 	static {
 		try {
 			InputStream stream = AbstractTest.class.getResourceAsStream("/gradle_info.properties");
-			if(stream == null) {
+			if (stream == null) {
 				throw new IllegalStateException("Run this one more time (AbstractTest#main)");
 			}
 			PROPERTIES.load(stream);
@@ -75,19 +81,20 @@ public class AbstractTest {
 		AbstracterUtil.registerDefaultConstants(Blocks.class, Items.class);
 		AbstracterConfig.registerConstants(Material.class,
 				c -> new ConstantsAbstracter(c, "v0/io/github/astrarre/block/Materials"));
-
 		AbstracterUtil.registerConstantlessInterface(Material.class);
-
+		registerSubclassBaseInterface(Block.class);
+		registerSubclassBaseInterface(Item.class);
+		registerSubclassBaseInterface(Enchantment.class);
+		registerSubclassBaseInterface(Entity.class);
+		registerSubclassBaseInterface(BlockEntity.class);
+		registerSubclassInterface(Tag.class);
 		AbstracterUtil.registerDefaultInterface(Block.class,
-				Item.class,
 				ItemStack.class,
 				Item.Settings.class,
 				BlockState.class,
 				BlockPos.class,
 				World.class,
 				WorldAccess.class,
-				Entity.class,
-				Enchantment.class,
 				Bootstrap.class,
 				StatusEffectInstance.class,
 				MinecraftClient.class,
@@ -96,9 +103,11 @@ public class AbstractTest {
 				MaterialColor.class,
 				Vec3d.class,
 				Vec3i.class,
-				EntityPose.class);
+				EntityPose.class,
+				SaddledComponent.class,
+				ItemSteerable.class);
 		// base
-		AbstracterUtil.registerDefaultBase(Block.class, Entity.class, Enchantment.class, Item.class, Material.class);
+		AbstracterUtil.registerDefaultBase(Material.class);
 
 		File folder = new File(PROPERTIES.getProperty("projectDir"), "generated");
 		folder.mkdirs();
@@ -108,5 +117,21 @@ public class AbstractTest {
 				new File(folder, "impl.jar").getAbsolutePath(),
 				new File(folder, "manifest.properties").getAbsolutePath(),
 				PROPERTIES.getProperty("mappings"));
+	}
+
+	private static void registerSubclassInterface(Class<?> cls) {
+		AbstracterUtil.registerDefaultInterface(cls);
+		for (Class<?> sub : REFLECTIONS.getSubTypesOf(cls)) {
+			AbstracterUtil.registerDefaultInterface(sub);
+		}
+	}
+
+	private static void registerSubclassBaseInterface(Class<?> sup) {
+		AbstracterUtil.registerDefaultInterface(sup);
+		AbstracterUtil.registerDefaultBase(sup);
+		for (Class<?> cls : REFLECTIONS.getSubTypesOf(sup)) {
+			AbstracterUtil.registerDefaultInterface(cls);
+			AbstracterUtil.registerDefaultBase(cls);
+		}
 	}
 }
--- src/main/java/io/github/astrarre/asm/AbstractionApplicator.java
+++ src/main/java/io/github/astrarre/asm/AbstractionApplicator.java
@@ -1,6 +1,17 @@
 package io.github.astrarre.asm;
 
 public class AbstractionApplicator implements Runnable {
+	private static final int TEST = 4;
+	public static final int NOT_TEST = internal();
+
+	private static int internal() {
+		return 0;
+	}
+
+	public static void publicApi() {
+		// e
+	}
+
 	@Override
 	public void run() {
 		// todo implement MM, should this be in the root project?
