b613185833b3b56cc53861148a6d94263059ec6c
Devan-Kerman <dev.sel20@gmail.com>
2021-04-24 10:27:09 -0500

    transfer tutorial png 1
    Signed-off-by: Devan-Kerman <dev.sel20@gmail.com>

--- astrarre-transfer-v0/src/testmod/java/testmod/TestModMain.java
+++ astrarre-transfer-v0/src/testmod/java/testmod/TestModMain.java
@@ -3,9 +3,11 @@ package testmod;
 import io.github.astrarre.access.v0.api.Access;
 import io.github.astrarre.access.v0.fabric.provider.ItemProvider;
 import io.github.astrarre.itemview.v0.fabric.ItemKey;
+import io.github.astrarre.transfer.v0.api.Droplet;
 import io.github.astrarre.transfer.v0.api.Insertable;
 import io.github.astrarre.transfer.v0.api.Participant;
 import io.github.astrarre.transfer.v0.api.ReplacingParticipant;
+import io.github.astrarre.transfer.v0.api.ad.AstrarreTransferTooltippedItem;
 import io.github.astrarre.transfer.v0.api.player.PlayerParticipant;
 import io.github.astrarre.transfer.v0.api.transaction.Key;
 import io.github.astrarre.transfer.v0.api.transaction.Transaction;
@@ -18,6 +20,7 @@ import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
@@ -32,6 +35,24 @@ public class TestModMain implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("fluid_cell"), ITEM);
+		Registry.register(Registry.ITEM, new Identifier("test_item"), new MyItem(new Item.Settings()));
+	}
+
+	public static class MyItem extends AstrarreTransferTooltippedItem /*just Item works as well*/ {
+		public MyItem(Settings settings) {
+			super(settings);
+		}
+
+		@Override
+		public ActionResult useOnBlock(ItemUsageContext context) {
+			World world = context.getWorld();
+			if (!world.isClient) {
+				Participant<Fluid> participant = FabricParticipants.FLUID_WORLD.get().get(context.getSide(), world, context.getBlockPos());
+				int inserted = participant.insert(Transaction.GLOBAL, Fluids.WATER, Droplet.BOTTLE);
+				context.getPlayer().sendMessage(new LiteralText("Inserted " + inserted + "dp"), false);
+			}
+			return ActionResult.CONSUME;
+		}
 	}
 
 	public static class FluidCellItem extends Item implements ItemProvider {
@@ -54,8 +75,7 @@ public class TestModMain implements ModInitializer {
 			if (!world.isClient) {
 				ReplacingParticipant<ItemKey> participant = ((PlayerParticipant) FabricParticipants.FROM_INVENTORY.get()
 				                                                                                                  .apply(null,
-						                                                                                                  context.getPlayer().inventory))
-						                                            .getHandReplacingParticipant(context.getHand());
+						                                                                                                  context.getPlayer().inventory)).getHandReplacingParticipant(context.getHand());
 				Participant<Fluid> fluidCell = FabricParticipants.FLUID_ITEM.get().get(null, ItemKey.of(context.getStack()), 1, participant);
 				fluidCell.insert(Transaction.GLOBAL, Fluids.WATER, 100);
 				System.out.println(fluidCell.insert(Transaction.GLOBAL, Fluids.LAVA, 100));
@@ -105,7 +125,7 @@ public class TestModMain implements ModInitializer {
 			if (fluid == Fluids.EMPTY || fluid == type) {
 				int count = current.getTag().getInt("amount") + quantity;
 				try (Transaction action = Transaction.create()) {
-					ItemKey newKey = current.withTag(current.getTag().toBuilder().putInt("amount", count));
+					ItemKey newKey = current.withTag(current.getTag().toBuilder().putInt("amount", count).putString("fluid", Registry.FLUID.getId(type).toString()));
 					if (!this.context.replace(action, current, 1, newKey, 1)) {
 						action.abort();
 						return 0;
