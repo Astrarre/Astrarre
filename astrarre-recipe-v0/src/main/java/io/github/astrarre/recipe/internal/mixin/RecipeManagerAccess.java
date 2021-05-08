672b8e42c8158910e253ae4504453f8c628e6ed7
Devan-Kerman <dev.sel20@gmail.com>
2021-05-08 09:22:09 -0400

    fix kjs compatibility
    Signed-off-by: Devan-Kerman <dev.sel20@gmail.com>

---
+++ astrarre-recipe-v0/src/main/java/io/github/astrarre/recipe/internal/mixin/RecipeManagerAccess.java
@@ -0,0 +1,16 @@
+package io.github.astrarre.recipe.internal.mixin;
+
+import java.util.Map;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.util.Identifier;
+
+@Mixin(RecipeManager.class)
+public interface RecipeManagerAccess {
+	@Accessor Map<RecipeType<?>, Map<Identifier, Recipe<?>>> getRecipes();
+}
---
+++ astrarre-recipe-v0/src/main/java/io/github/astrarre/recipe/internal/mixin/SinglePreperationResourceReloadListenerMixin_RecipePostReloadEvent.java
@@ -0,0 +1,22 @@
+package io.github.astrarre.recipe.internal.mixin;
+
+import io.github.astrarre.recipe.v0.fabric.RecipePostReloadEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceReloadListener;
+import net.minecraft.resource.SinglePreparationResourceReloadListener;
+import net.minecraft.util.profiler.Profiler;
+
+@Mixin(SinglePreparationResourceReloadListener.class)
+public abstract class SinglePreperationResourceReloadListenerMixin_RecipePostReloadEvent implements ResourceReloadListener {
+	@Inject(method = "method_18790", at = @At("RETURN"))
+	public void onPostReload(ResourceManager manager, Profiler profiler, Object object) {
+		if((Object)this instanceof RecipeManager) {
+			RecipePostReloadEvent.EVENT.get().onReload((RecipeManager) (Object) this, ((RecipeManagerAccess)this).getRecipes());
+		}
+	}
+}
