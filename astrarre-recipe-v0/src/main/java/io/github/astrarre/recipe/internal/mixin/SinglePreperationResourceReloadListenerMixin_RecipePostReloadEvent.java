1b35ae5b6a2ba9aa74b186aa21fcf0a14b399f17
Devan-Kerman <dev.sel20@gmail.com>
2021-05-09 17:04:31 -0500

    fix mixin error
    Signed-off-by: Devan-Kerman <dev.sel20@gmail.com>

--- astrarre-recipe-v0/src/main/java/io/github/astrarre/recipe/internal/mixin/SinglePreperationResourceReloadListenerMixin_RecipePostReloadEvent.java
+++ astrarre-recipe-v0/src/main/java/io/github/astrarre/recipe/internal/mixin/SinglePreperationResourceReloadListenerMixin_RecipePostReloadEvent.java
@@ -4,6 +4,7 @@ import io.github.astrarre.recipe.v0.fabric.RecipePostReloadEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.recipe.RecipeManager;
 import net.minecraft.resource.ResourceManager;
@@ -14,7 +15,7 @@ import net.minecraft.util.profiler.Profiler;
 @Mixin(SinglePreparationResourceReloadListener.class)
 public abstract class SinglePreperationResourceReloadListenerMixin_RecipePostReloadEvent implements ResourceReloadListener {
 	@Inject(method = "method_18790", at = @At("RETURN"))
-	public void onPostReload(ResourceManager manager, Profiler profiler, Object object) {
+	public void onPostReload(ResourceManager manager, Profiler profiler, Object object, CallbackInfo info) {
 		if((Object)this instanceof RecipeManager) {
 			RecipePostReloadEvent.EVENT.get().onReload((RecipeManager) (Object) this, ((RecipeManagerAccess)this).getRecipes());
 		}
